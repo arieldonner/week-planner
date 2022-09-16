0c42244422e7edef55938ad2af5c3425ca52e62a
Brandon Au <design.brandonau@gmail.com>
2022-09-16 19:21:01 +0000

    added event listeners for submit btn + form and started table html

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // homepage with Weekly Planner title and center button
 //
+
+var $addEntry = document.querySelector('.add-btn');
+var $modal = document.querySelector('.overlay');
+var $entryForm = document.querySelector('.entry-form');
+
+$addEntry.addEventListener('click', handleAddEntry);
+function handleAddEntry(event) {
+  $modal.className = 'overlay';
+}
+
+$entryForm.addEventListener('submit', handleEntrySubmit);
+function handleEntrySubmit(event) {
+  event.preventDefault();
+  $modal.className = 'overlay hidden';
+}
